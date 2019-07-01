5cd7324039bac4aad52177c7f6f4fc5ab7c8054e
sbuteryd@qq.com <sbuteryd@qq.com>
2019-07-01 13:08:06 +0800

    添加状态 返回状态

---
+++ first/index.js
@@ -0,0 +1,12 @@
+function createStorm() {
+//    1 state
+//    2 get state
+//    3 listening state
+//    4 update state
+    let state
+    const getState =()=> state;
+    return(
+        getState
+    )
+}
+
