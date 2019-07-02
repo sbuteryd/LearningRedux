daaa758b79d031f244760bd6c43fcccec0064060
sbuteryd@qq.com <sbuteryd@qq.com>
2019-07-02 11:28:16 +0800

    设置监听状态

--- second/index.js
+++ second/index.js
@@ -1,13 +1,28 @@
-function store() {
+function createStore() {
 //    状态树需要4个部分
 //    1. 状态
 //    2. 获取状态
 //    3. 监听状态
 //    4. 更新状态
-    let state
+    let state;
+    let listeners=[];
     const getState = ()=> state;
 
+    const subscribe = listener =>{
+        listeners.push(listener)
+    };
     return{
         getState,
+        subscribe
     }
 }
+
+const store = createStore();
+
+store.subscribe(()=>{
+    console.log('The new state is',store.getState())
+});
+
+const unsubscribe = store.subscribe(()=>{
+    console.log('The store change')
+});
